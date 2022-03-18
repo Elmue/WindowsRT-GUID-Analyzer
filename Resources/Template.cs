fbe70bd2db988058a0fd8954fb994731297f89b1
Elmue <elmue@gmx.de>
2022-03-18 15:32:11 -0300

    Add files via upload

---
+++ Resources/Template.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#if DEBUG
+public class Debugging
+{{
+	private static Dictionary<String,String> mi_Interfaces = new Dictionary<String,String>();
+
+	public static String GetInterfaceName(String s_Guid)
+	{{
+		String s_Name;
+		mi_Interfaces.TryGetValue(s_Guid.ToUpper(), out s_Name);
+		return s_Name;
+	}}
+	
+	// {1}
+	static Debugging()
+	{{
+{2}
+	}}
+}}
+#endif // DEBUG
---
+++ Resources/Template.h
@@ -0,0 +1,35 @@
+﻿#include <afxcoll.h>
+#include <assert.h>
+
+#if _DEBUG
+class Debugging
+{{
+public:
+	static CString GetInterfaceName(CString s_Guid)
+	{{
+		// You could call Init() here but this class would not be thread safe anymore.
+		assert(!Interfaces().IsEmpty());
+		
+		s_Guid.MakeUpper();
+		CString s_Name;
+		Interfaces().Lookup(s_Guid, s_Name);
+		return s_Name;
+	}}
+
+	// {1}
+	static void Init()
+	{{
+		if (!Interfaces().IsEmpty()) return;
+
+{2}		
+	}}
+	
+private:
+	__forceinline static CMapStringToString& Interfaces()
+	{{ 
+		// Making the variable local avoids the need to declare a static class member in an additional CPP file.
+		static CMapStringToString i_Map;
+		return i_Map;
+	}}
+}};
+#endif // _DEBUG
